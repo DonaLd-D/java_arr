9fdf0b34e1a231a1fa43ba1abe7a04be7435e636
DonaLd-D <57483340+DonaLd-D@users.noreply.github.com>
2021-01-29 15:06:04 +0800

    java array

---
+++ src/com/company/jarr/FillClass.java
@@ -0,0 +1,18 @@
+package com.company.jarr;
+
+import java.util.*;
+
+public class FillClass {
+    public static void main(String args[]) {
+        int array[] = new int[6];
+        Arrays.fill(array, 100);
+        for (int i=0, n=array.length; i < n; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println();
+        Arrays.fill(array, 3, 6, 50);
+        for (int i=0, n=array.length; i< n; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
